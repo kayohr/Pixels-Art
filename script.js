30a33a18840755c757884971126f35f3c1460826
kayo <kayohramos@gmail.com>
2022-08-04 16:13:40 -0300

    exercicios 1-7

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let elementLi = document.querySelectorAll('li');
+
 //https://www.youtube.com/watch?v=wZZyhrJxZRU
 function square(select) {
   /*pixelBoard.style.setProperty('--select', select);*/
@@ -10,22 +12,22 @@ function square(select) {
 }
 square(5);
 
+function add_remover(novo_selecionado){
+  let elemento_selecionado=document.getElementsByClassName('selected');
+elemento_selecionado[0].classList.remove('selected');
+novo_selecionado.classList.add('selected');
+}
 
-function add_remover() {
-  let cores = document.querySelector('color-palette');
-  for (let i = 0; i < cores.length - 1; i += 1) {
-    if (cores[i] === cores) {
-      cores[i].classList.add('selected');
-    } else {
+
+
+/*function add_remover(alteracaoDeItem) {
+  let cores = document.getElementsByClassName('color');
+  for (let i = 0; i < cores.length; i += 1) {
+    if (cores[i].classList.contains('selected')) {
       cores[i].classList.remove('selected');
+    } else {
+      alteracaoDeItem.classList.add('selected');
     }
   }
 }
-/*function selecaoDeCores() {
-  for (let i = 0; i < cores.length - 1; i += 1) {
-    cores[i].addEventListener('click', () => {
-      add_remover[i];
-    });
-  }
-}*/
-
+add_remover();*/
