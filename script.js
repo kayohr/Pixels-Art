9c5ff12dcb497684d982990f7662f363ad6bb2d7
kayo <kayohramos@gmail.com>
2022-08-03 21:02:15 -0300

    tentando fazer a 7

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let pixelBoard = document.querySelector('#pixel-board');
-
+//https://www.youtube.com/watch?v=wZZyhrJxZRU
 function square(select) {
   /*pixelBoard.style.setProperty('--select', select);*/
+  let pixelBoard = document.querySelector('#pixel-board');
   for (let i = 0; i < select * select; i += 1) {
     let div = document.createElement('div');
     div.classList.add('pixel');
@@ -9,3 +9,23 @@ function square(select) {
   }
 }
 square(5);
+
+
+function add_remover() {
+  let cores = document.querySelector('color-palette');
+  for (let i = 0; i < cores.length - 1; i += 1) {
+    if (cores[i] === cores) {
+      cores[i].classList.add('selected');
+    } else {
+      cores[i].classList.remove('selected');
+    }
+  }
+}
+/*function selecaoDeCores() {
+  for (let i = 0; i < cores.length - 1; i += 1) {
+    cores[i].addEventListener('click', () => {
+      add_remover[i];
+    });
+  }
+}*/
+
